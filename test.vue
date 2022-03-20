84a9878ed7227966bcd8484fab24f858ed2ae29c
pizazz <1201741224@cug.edu.cn>
2022-03-20 20:04:57 +0800

    111

---
+++ test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app" @click="sayHello">app</div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        method:{
+            sayHello(){
+                alert("你好呀")
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #app{
+        background-color: #42b983;
+    }
+</style>
